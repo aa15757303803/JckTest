17cc419dbd59a3a71531d06769f7bcdef733d22e
jck <815843571@qq.com>
2018-11-30 00:43:36 +0800

    序列化依赖jar包

--- src/main/java/com/jck/dao/cache/RedisDao.java
+++ src/main/java/com/jck/dao/cache/RedisDao.java
@@ -1,10 +1,33 @@
 package com.jck.dao.cache;
 
+import com.jck.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 public class RedisDao {
 
     private JedisPool jedisPool;
 
+    private static final Logger logger = LoggerFactory.getLogger(RedisDao.class);
 
+    public RedisDao(String ip, int port) {
+        jedisPool = new JedisPool(ip, port);
+    }
+
+    public User getUser(long userId) {
+        try {
+            Jedis jedis = jedisPool.getResource();
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("Redis获取User用户错误!:"+e.getMessage());
+        }
+        return null;
+    }
+
+    public String putUser(User user) {
+        return null;
+    }
 }
